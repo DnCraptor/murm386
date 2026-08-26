a48b2daf315650188e2911506fe32cb15b9d5d2e
Mike Vengerovich <mikeveng73@gmail.com>
2026-08-26 23:56:11 +0300

    + v.1.14

--- src/fdos/fdos.h
+++ src/fdos/fdos.h
@@ -6,7 +6,8 @@
 
 #ifndef __hfa_func
 #define __hfa(group) __attribute__((section(".hfa." group)))
-#define __hfa_func(func_name) __hfa(__STRING(func_name)) func_name
+#define __hfa_func(func_name) func_name
+// __hfa(__STRING(func_name))
 #endif
 
 void _boot(CPU*); // like kernel/boot/boot.asm
